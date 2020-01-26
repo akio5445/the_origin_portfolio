9a12266716663df905b81b63bfa38c3abd5d331e
akio5445 <57023274+akio5445@users.noreply.github.com>
2020-01-26 22:06:33 +0900

    Userモデルを追加
    *

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :articles, dependent: :destroy
   # 多くの記事コメントと関連、諸共削除
   has_many :article_comments, dependent: :destroy
+
   # セキュアにハッシュ化したパスワードを、 データベース内のpassword_digestという属性に保存できるようになる。
   # 2つのペアの仮想的な属性（passwordとpassword_confirmation）が使えるようになる。
   # また、存在性と値が一致するかどうかのバリデーションも追加される 。
