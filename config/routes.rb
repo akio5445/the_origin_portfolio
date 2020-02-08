3a09c5690b0e7576ae0f4a9bb33682d12b3d6083
akio5445 <57023274+akio5445@users.noreply.github.com>
2020-02-08 20:24:54 +0900

    routes.rbリファクタリング
    * resourceにしてコンパクトに

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,13 @@ Rails.application.routes.draw do
 # Aboutページ
   root "top#index"
 # loginページ
-  get    '/login',   to: 'sessions#new', as: :login
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
-# 月別アーカイブページ ' /user/X/archives/20XX0X0X '
-  get  '/users/:id/archives/:yyyymm', to: 'users#archives', as: :user_archive
-
-  resources :users
+  get    "/login",   to: "sessions#new", as: :login
+  resource :sessions, only: [:create, :destroy]
+  resources :users, only: [ :show, :new, :create ]
   resources :articles do
-    resources :article_comments
+    resources :article_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
+# 月別アーカイブページ " /user/X/archives/20XX0X0X "
+  get  "/users/:id/archives/:yyyymm", to: "users#archives", as: :user_archive
 end
