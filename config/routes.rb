26c821911a9dd34266c8204772f3bdc50ccce09a
akio5445 <57023274+akio5445@users.noreply.github.com>
2020-02-08 20:46:13 +0900

    routes.rb修正
    * 仕様を一部戻した

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
 # Aboutページ
   root "top#index"
-# loginページ
+
   get    "/login",   to: "sessions#new", as: :login
-  resource :sessions, only: [:create, :destroy]
+  post   "/login",   to: "sessions#create"
+  delete "/logout",  to: "sessions#destroy"
+
   resources :users, only: [ :show, :new, :create ]
   resources :articles do
-    resources :article_comments, only: [:create, :destroy]
+    resource :article_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
+
 # 月別アーカイブページ " /user/X/archives/20XX0X0X "
   get  "/users/:id/archives/:yyyymm", to: "users#archives", as: :user_archive
 end
