5408ed7294c41bea20470aeafcf997e7921e01da
akio5445 <57023274+akio5445@users.noreply.github.com>
2020-02-12 11:35:56 +0900

    修正
    * create_at => created_at
    * url("img") => asset_path("images/img")

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,9 +1,6 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-  # カスタムクエリー用メソッド(新しい順に取得)
-  scope :recent, -> { order(create_at: :desc) }
-
-  # creatd_atをもとに月別に記事を集計するメソッド
+  # created_atをもとに月別に記事を集計するメソッド
   def make_archive
     return self.articles.group("strftime('%Y%m', articles.created_at)")
                .order(Arel.sql("strftime('%Y%m', articles.created_at) desc"))
