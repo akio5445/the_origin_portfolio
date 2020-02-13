eec94eb73e941ff0c83a6ca8844d9c42565508ed
akio5445 <57023274+akio5445@users.noreply.github.com>
2020-02-13 13:12:05 +0900

    fix

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,9 +1,9 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
   # created_atをもとに月別に記事を集計するメソッド
-  #def make_archive
-  #  return self.articles.group("strftime('%Y%m', articles.created_at)")
-  #             .order(Arel.sql("strftime('%Y%m', articles.created_at) desc"))
-  #             .count
-  #end
+  def make_archive
+    return self.articles.group("strftime('%Y%m', articles.created_at)")
+               .order(Arel.sql("strftime('%Y%m', articles.created_at) desc"))
+               .count
+  end
 end
