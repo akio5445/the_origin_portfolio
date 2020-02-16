23efdc05c8e8ca6bbd4c5b6f728b4ff4b17b341b
akio5445 <57023274+akio5445@users.noreply.github.com>
2020-02-16 20:44:50 +0900

    Remove past log display function
    * 月別過去ログ機能関連を削除

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,3 @@ module ApplicationHelper
   include SessionsHelper
   include MarkdownHelper
 end
-
-  ## viewの表示を20XX年XX月(X)日にする
-  #def ymconv(yyyymm,cnt)
-  #  yyyy = yyyymm[0,4]
-  #  mm = yyyymm[4,2]
-  #  return yyyy + "年" + mm + "月 (" + cnt + ")"
-  #end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,12 +1,2 @@
 module UsersHelper
 end
-
-  #def ymconvn(yyyymm)
-  #  yyyy = yyyymm[0,4]
-  #  mm = yyyymm[4,2]
-  #  return yyyy + "年" + mm + "月 "
-  #end
-  #月別の記事数を処理
-  #def ymconv(yyyymm,cnt)
-  #  return yyyymm + " (" + cnt + ")"
-  #end
