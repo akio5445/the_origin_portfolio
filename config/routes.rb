2c09bb4dd4326b46db7fbaa177340446939851cf
akio5445 <57023274+akio5445@users.noreply.github.com>
2020-02-18 23:42:34 +0900

    Fix category

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
 # Aboutページ
   root "top#index"
-
   get    "/login",   to: "sessions#new", as: :login
   post   "/login",   to: "sessions#create"
   delete "/logout",  to: "sessions#destroy"
-　get '/article_categories/:id/show', to: "articles#category",  as: :category
   resources :users, only: [ :show, :new, :create ]
   resources :articles do
     resources :article_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
+  get '/article_categories/:id/show', to: "articles#category",  as: :category
 end
