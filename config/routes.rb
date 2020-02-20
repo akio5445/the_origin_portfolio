93cc0d676e4888462a6f3ccc5971065e0845c431
akio5445 <57023274+akio5445@users.noreply.github.com>
2020-02-20 18:28:51 +0900

    Add category * カテゴリーをサイドバーに追加 クリック先に関連記事が表示されるページを追加 * 各所パーシャル化

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,8 @@ Rails.application.routes.draw do
     resources :article_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
+  # カテゴリータグ移行先のそのタグが含まれる記事が表示されるページ
   get '/article_categories/:id/show', to: "articles#category",  as: :category
+  # サイドバーカテゴリータグ移行先のそのタグが含まれる記事が表示されるページ
+  get '/article_category/:id/:name', to: "articles#each_category",  as: :each_category
 end
