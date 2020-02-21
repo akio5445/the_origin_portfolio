3f787abec6eff264e5a437568a3a9b83e6c85c31
akio5445 <57023274+akio5445@users.noreply.github.com>
2020-02-21 14:04:32 +0900

    Fix markdown * マークダウンで記述した際にエラーが出ることがあるのを修正 * gemをcoderayからrougeに変更 * それに伴い各ページの記述を変更 ＊ markdown.helperを修正

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -1,43 +1,59 @@
+require 'rouge/plugins/redcarpet'
+
+class HTML < Redcarpet::Render::HTML
+  include Rouge::Plugins::Redcarpet
+end
+
 module MarkdownHelper
-  require "redcarpet"
-  require "coderay"
-  class HTMLwithCoderay < Redcarpet::Render::HTML
-    def block_code(code, language)
-      language = language.split(':')[0]
+  def markdown(text)
+    options = {
+      # htmlを出力しない
+      filter_html:   true,
+      # <img>タグを生成しない
+      no_images:     true,
+      # <style>タグを生成しない
+      no_styles:     true,
+      # 見出しにアンカーを付ける
+      with_toc_data: true,
+      # 改行を<br>タグに変換
+      hard_wrap:     true,
+      # 題名を表すハッシュ(#)と文字の間にスペースが必要
+      space_after_headers: true
+    }
 
-      case language.to_s
-      when 'rb'
-        lang = 'ruby'
-      when 'yml'
-        lang = 'yaml'
-      when 'css'
-        lang = 'css'
-      when 'html'
-        lang = 'html'
-      when ''
-        lang = 'md'
-      else
-        lang = language
-      end
+    extensions = {
+      # <>で囲まれていなくてもリンクを認識する
+      autolink:           true,
+      # フェンスのあるコードブロックを認識する
+      fenced_code_blocks: true,
+      # HTMLブロックを空行で囲む必要はない
+      lax_spacing:        true,
+      # 単語中の強調を強調と認識しない
+      no_intra_emphasis:  true,
+      # ~(チルダ)2つで取り消し線を表現する
+      strikethrough:      true,
+      # の後ろが上付き文字になる(文章が少し上に表示される)
+      superscript:        true,
+      # テーブルを認識する
+      tables:             true
+    }
 
-      CodeRay.scan(code, lang).div
+    unless @markdown
+      # Redcarpet::Render::HTML => HTML に変更
+      renderer = HTML.new(options)
+      @markdown = Redcarpet::Markdown.new(renderer, extensions)
     end
+
+    @markdown.render(text).html_safe
   end
 
-  def markdown(text)
-    html_render = HTMLwithCoderay.new(filter_html: true, hard_wrap: true)
-    options = {
-        autolink: true,
-        space_after_headers: true,
-        no_intra_emphasis: true,
-        fenced_code_blocks: true,
-        tables: true,
-        hard_wrap: true,
-        xhtml: true,
-        lax_html_blocks: true,
-        strikethrough: true
+  def toc(text)
+    toc_option = {
+      nesting_level: 1
     }
-    markdown = Redcarpet::Markdown.new(html_render, options)
-    markdown.render(text)
+
+    toc_renderer = Redcarpet::Render::HTML_TOC.new
+    toc = Redcarpet::Markdown.new(toc_renderer, toc_option)
+    toc.render(text).html_safe
   end
 end
